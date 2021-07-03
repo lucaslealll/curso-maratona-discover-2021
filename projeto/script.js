5523973dc526a0235e5e98d8b729dec2b74db1eb
lucasoal <lucasoalzst@gmai.com>
2021-07-03 17:59:29 -0300

    New transaction button and Cancel button are working

--- projeto/script.js
+++ projeto/script.js
@@ -1,54 +1,18 @@
 const Modal = {
-    // METODO OPEN
-    open() {
-        // Abrir modal
-        // add a class active ao modal
-        document
-            .querySelector('.modal-overlay')
-            .classList
-            .add('active')
-    },
-    // METODO CLOSE
-    close() {
-        // Fechar o modal
-        // remover a class active do modal
-        document
-            .querySelector('.modal-overlay')
-            .classList.remove('active')
-    }
+        // CRIAÇÃO DE UM MÉTODO (FUNCTION)
+        open() {
+                // OPEN Modal
+                // ADD CLASS ACTIVE AT Modal
+                document
+                        .querySelector('.modal-overlay')
+                        .classList.add('active')
+
+        },
+        close() {
+                // CLOSE Modal
+                // REMOVE CLASS ACTIVE
+                document
+                        .querySelector('.modal-overlay')
+                        .classList.remove('active')
+        }
 }
-
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    }
-]
-
-const Transaction = {
-    incomes() {
-        // somar as entradas
-    },
-    expenses() {
-        // somar as saídas
-    },
-    total() {
-        // entradas - saídas
-    }
-
-}
-
